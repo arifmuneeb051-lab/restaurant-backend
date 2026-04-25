3491ff59c6fbfe28163f2ed7bae8d366cab572b1
arifmuneeb051-lab <arifmunee051@gmail.com>
2026-04-25 15:03:32 +0500

    fix port for railway

--- server.js
+++ server.js
@@ -113,6 +113,8 @@ app.post("/login", (req, res) => {
 // ==========================
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
