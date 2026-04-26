f426031f57feebe4e3e6c03a3a9fcccaccc3e952
arifmuneeb051-lab <arifmunee051@gmail.com>
2026-04-26 11:02:34 +0500

    fix login bug

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   const user = users.find(
-    u => u.username === username && u.password === password
+    (u) => u.username === username && u.password === password
   );
 
   if (user) {
